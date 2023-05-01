fe49e22b5929f1e7ab9a272de5effc525d46953a
TheKrainBow <thekrainbow@gmail.com>
2023-05-01 02:01:01 +0200

    feat: starting network

---
+++ Assets/Scripts/RPCTest.cs
@@ -0,0 +1,29 @@
+using Unity.Netcode;
+
+public class RpcTest : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        if (!IsServer && IsOwner) //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
+        {
+            TestServerRpc(0, NetworkObjectId);
+        }
+    }
+
+    [ClientRpc]
+    void TestClientRpc(int value, ulong sourceNetworkObjectId)
+    { 
+        UnityEngine.Debug.Log($"Client Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        if (IsOwner) //Only send an RPC to the server on the client that owns the NetworkObject that owns this NetworkBehaviour instance
+        {
+            TestServerRpc(value + 1, sourceNetworkObjectId);
+        }
+    }
+
+    [ServerRpc]
+    void TestServerRpc(int value, ulong sourceNetworkObjectId)
+    {
+        UnityEngine.Debug.Log($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        TestClientRpc(value, sourceNetworkObjectId);
+    }
+}
